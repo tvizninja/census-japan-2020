15888987ff38425ababd6fbcb0cf090a14e5d7cb
tvizninja <github@tviz.ninja>
2022-07-29 17:28:22 +0000

    add past files(2005,2010,2015) to 02.addlatlng

--- 02.addlatlng/script.py
+++ 02.addlatlng/script.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # script.py: meshnum to latlng
 
+import os
+import glob
 import pandas as pd
 
 def m3tolatlng (m):
@@ -15,8 +17,10 @@ def m5tolatlng (m):
     lng = int(m[2:4])+100 + int(m[5:6])/8  + int(m[7:8])/80  + ((int(m[8:9])-1)%2)/160 + ((int(m[9:10])-1)%2)/320 + 1/640
     return (lat, lng)
 
-df = pd.read_csv('../01.download/m5pop-2020.csv.gz', sep=',')
-df['lat'] = df['KEY_CODE'].apply(lambda x: m5tolatlng(x)[0])
-df['lng'] = df['KEY_CODE'].apply(lambda x: m5tolatlng(x)[1])
-#df.to_csv('m5pop-2020-latlng.csv')
-df.to_csv('m5pop-2020-latlng.csv.gz')
+for f in glob.glob("../01.download/*.gz"):
+    f = os.path.normpath(f)
+    print(f)
+    df = pd.read_csv(f, sep=',')
+    df['lat'] = df['KEY_CODE'].apply(lambda x: m5tolatlng(x)[0])
+    df['lng'] = df['KEY_CODE'].apply(lambda x: m5tolatlng(x)[1])
+    df.to_csv(f.split(os.sep)[-1].replace(r".csv", "-latlng.csv"), index=False)
